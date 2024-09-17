8d0e0435de2e5151da8677dd209bd1168b35bfc0
LisaTarn <150932484+LisaTarn@users.noreply.github.com>
2024-09-17 15:26:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Example: Declaring and using a string variable
+let studentName = "Lisa Tarnow";
+console.log(studentName); // This should print "Lisa Tarnow" in the console
+ // Example: Declaring and using a numeric variable
+let studentAge = 40;
+console.log(studentAge); // This should print 40 in the console
+ // Example: Declaring and using a boolean variable
+let isEnrolled = true;
+console.log(isEnrolled); // This should print true in the console
+ 
+// Example: Declaring and using a string variable
+let firstName = "Lisa";
+console.log(firstName); // This should print "Lisa" in the console
+ // Example: Declaring and using a numeric variable
+let birthYear = 1983;
+console.log(birthYear); // This should print 1983 in the console
+ // Example: Declaring and using a boolean variable
+let isFemale = true;
+console.log(isFemale); // This should print true in the console
+// Example: Declaring and using an array
+let vegetables = ['Brocolli', 'Arugula', 'Spinach'];
+console.log(vegetables); // This should print "Brocolli, Arugula, Spinach" in the console
+// Example: Declaring and using an object
+let birthDay = {
+    month: "October",
+    day: 23,
+    year: 1983,
+  };
+  
+console.log(birthDay); // This should print October 23 1983
+
+console.log(5 + 3); // Addition
+console.log(6 - 2); //Subtraction
+console.log(4 * 3); //Multiplication
+console.log(6 / 3); //Division
+
+let x = 6
+let y = 12
+
+console.log(x > y); // Example of comparison operator > will print false
+console.log(x < y); // Example of comparison operator < will print true
+console.log (x === y); // Example of comparison operator === will print false
+console.log (x !== y); // Example of comparison operator !== will print true
+
+let a = true;
+let b = false;
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+console.log(!a); // Logical NOT
